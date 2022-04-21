2e41c51d62900d0ff14281b28eabf78d7e95daa6
Kevin Herink <kevinherink87@gmail.com>
2022-04-21 15:21:45 -0500

    Pseudo for coding challenge for 2022-04-20

--- CC 2022-04-20/app.js
+++ CC 2022-04-20/app.js
@@ -4,3 +4,14 @@
 // Example:
 // 348597 => [7,9,5,8,4,3]
 // 0 => [0]
+
+//P R E P
+//Parameters    - What are the parameters? integers? special characters? empty array?
+//Return        - What kind of return is expected? console.log? print to dom?
+//Example       - Are these examples correct? create 3 tests.
+//Pseudo code   - Create pseudo code for what you are about to write.
+
+// psuedo
+// make a function that takes in a number
+// place each number from the arguement into an array
+// reverse the array
