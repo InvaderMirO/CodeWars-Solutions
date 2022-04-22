547fa28ca2864215fd1020f65a262e4da23bf5d1
Kevin Herink <kevinherink87@gmail.com>
2022-04-22 15:52:41 -0500

    Attempts and Testing for coding challenge for 2022-04-20

--- CC 2022-04-20/app.js
+++ CC 2022-04-20/app.js
@@ -11,7 +11,24 @@
 //Example       - Are these examples correct? create 3 tests.
 //Pseudo code   - Create pseudo code for what you are about to write.
 
+//p: are you ever given an empty array or undefined? are you always given digits?
+//r: whatever number is given, it is now split up into an array and the order of the numbers is reversed.
+//e1: if given 9876 => [6,7,8,9]
+//e2: if given 455445 => [5,4,4,5,5,4]
+//e3: if given 0 => [0]
+
 // psuedo
 // make a function that takes in a number
-// place each number from the arguement into an array
-// reverse the array
+function reverseArray(num){
+    // make number a string and split that bebe then reverse it.
+    let numArray = num.toString().split('').reverse()
+    // make array integers again 
+    return parseInt(numArray) // doesn't work
+    return Number(numArray) // doesn't work
+    //and return
+    console.log(numArray)
+}
+
+reverseArray(9876)
+console.log(reverseArray(455445),[5,4,4,5,5,4])
+// cant do it this way.... need to use map...
