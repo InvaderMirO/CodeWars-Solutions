85f0dbd70c8eca2d5f37ed8203eee9be564da41d
Kevin Herink <kevinherink87@gmail.com>
2022-04-27 13:12:26 -0500

    Psuedo - Coding Challenge for 2022-06-01

--- CC 2022-06-01/app.js
+++ CC 2022-06-01/app.js
@@ -3,3 +3,10 @@
 // repeatStr(6, "I") // "IIIIII"
 // repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
 
+
+// take in params for number of repetitions and string.
+// loop the number of reptititions and return string each interation.
+function repeatStr (n, s) {
+    
+    return '';
+  }
