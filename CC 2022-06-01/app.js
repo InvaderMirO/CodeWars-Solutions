fbc465f4648ade338b1334cf52e4495c84ff957a
Kevin Herink <kevinherink87@gmail.com>
2022-04-29 13:27:54 -0500

    Solution - Coding Challenge for 2022-06-01

--- CC 2022-06-01/app.js
+++ CC 2022-06-01/app.js
@@ -3,9 +3,12 @@
 // repeatStr(6, "I") // "IIIIII"
 // repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
 
+//loop i initially attempted didn't work as i didn't create a variable for the return outside of the loop... dummy. see fix below in refactors.
 
 // take in params for number of repetitions and string.
 // return repeat function on string 
 function repeatStr (n, s) {
     return s.repeat(n)
 }
+
+// solution successful
