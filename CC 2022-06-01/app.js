d838a36f2928f8c4ce48112826d1c9033cc1ff34
Kevin Herink <kevinherink87@gmail.com>
2022-05-01 13:30:13 -0500

    Refactor 2 - Coding Challenge for 2022-06-01

--- CC 2022-06-01/app.js
+++ CC 2022-06-01/app.js
@@ -3,7 +3,13 @@
 // repeatStr(6, "I") // "IIIIII"
 // repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
 
-//loop i initially attempted didn't work as i didn't create a variable for the return outside of the loop... dummy. see fix below in refactors.
+//loop i initially attempted didn't work as i didn't create a variable for the return outside of the loop... dummy. see fix below
+function repeatStr (n, s) {
+    var str="";
+    for(var i=0; i < n; i++)
+      str+=s;
+      return str;
+    }
 
 // take in params for number of repetitions and string.
 // return repeat function on string 
