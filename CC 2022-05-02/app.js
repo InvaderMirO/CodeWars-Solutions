6cca905a3182cff1b3857857f005c9789d9f5f34
Kevin Herink <kevinherink87@gmail.com>
2022-05-03 13:53:31 -0500

    Psuedo - Coding Challenge for 2022-05-02

--- CC 2022-05-02/app.js
+++ CC 2022-05-02/app.js
@@ -5,3 +5,10 @@
 // Given [34, 15, 88, 2] your solution will return 2
 // Given [34, -345, -1, 100] your solution will return -345
 // You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+// need to sort array and return lowest int which will also include negatives.
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    
+  }
+}
