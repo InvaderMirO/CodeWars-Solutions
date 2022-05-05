9c8d19dbcc6fc8b979b7ea7a0a553e38aec4c6df
Kevin Herink <kevinherink87@gmail.com>
2022-05-05 14:08:09 -0500

    Solution - Coding Challenge for 2022-05-02

--- CC 2022-05-02/app.js
+++ CC 2022-05-02/app.js
@@ -8,8 +8,18 @@
 
 // need to sort array and return lowest int which will also include negatives.
 // returning math.min function on the array.
+// class SmallestIntegerFinder {
+//   findSmallestInt(args) {
+//     return Math.min(args) // doesn't work.
+//   }
+// }
+
+// https://medium.com/@vladbezden/how-to-get-min-or-max-of-an-array-in-javascript-1c264ec6e1aa
+// needed to use the ... in front of the args in order to convert array to distinct varaiables and send them to the function.
+
 class SmallestIntegerFinder {
   findSmallestInt(args) {
-    return Math.min(args)
+    return Math.min(...args)
   }
 }
+
