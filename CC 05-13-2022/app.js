c0c360ece61084c9664a5e053dc72683e73946fb
Kevin Herink <kevinherink87@gmail.com>
2022-05-13 14:08:48 -0500

    Coding Challenge and Solution for 5-13-22 coding challenge

---
+++ CC 05-13-2022/app.js
@@ -0,0 +1,23 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+//P R E P
+//Parameters    - What are the parameters? integers? special characters? empty array?
+//Return        - What kind of return is expected? console.log? print to dom?
+//Example       - Are these examples correct? create 3 tests.
+//Pseudo code   - Create pseudo code for what you are about to write.
+
+
+// psuedo
+// make a function that takes in an array
+// map through the array and multiply each element by 2 and return
+
+function doubled(array){
+    return array.map(element=>element*2);
+}
+
+doubled([1,2,3])
+console.log(doubled([2,3,4]),[4,6,8])
